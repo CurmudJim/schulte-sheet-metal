1081ac9f424bfa89c2262f28c838a0713d9f736d
CurmudJim <jgarvey86@gmail.com>
2017-11-22 13:16:53 -0600

    Added phone number

--- main.rb
+++ main.rb
@@ -1,4 +1,27 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'pony'
+
+def send_message
+  Pony.mail({
+    :from => params[:name] + "<" + params[:email] + ">",
+    :to => 'jgarvey86@gmail.com',
+    :subject => params[:name] + " has contacted you",
+    :body => params[:message],
+    # :port => '587',
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.gmail.com',
+      :port => '587',
+      :enable_starttls_auto => true,
+      :user_name => 'jgarvey86',
+      :password => 'cjohnson71$',
+      :authentication => :plain,
+      :domain => 'localhost.localdomain'
+      }
+    })
+  end
+
 
 get '/' do
   erb :home
@@ -6,6 +29,12 @@ end
 
 get '/contact' do
   erb :contact
+end
+
+post '/contact' do
+  send_message
+  # flash[:notice] = "Thank you for contacting us. We'll be in touch soon."
+  # redirect to('/')
 end
 
 not_found do
