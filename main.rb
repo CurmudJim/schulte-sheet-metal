a9581141d279a50018e835032908ebb90506268f
CurmudJim <jgarvey86@gmail.com>
2017-11-22 13:20:16 -0600

    Did not add phone number

--- main.rb
+++ main.rb
@@ -1,26 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'pony'
+# require 'pony'
 
-def send_message
-  Pony.mail({
-    :from => params[:name] + "<" + params[:email] + ">",
-    :to => 'jgarvey86@gmail.com',
-    :subject => params[:name] + " has contacted you",
-    :body => params[:message],
-    # :port => '587',
-    :via => :smtp,
-    :via_options => {
-      :address => 'smtp.gmail.com',
-      :port => '587',
-      :enable_starttls_auto => true,
-      :user_name => 'jgarvey86',
-      :password => 'cjohnson71$',
-      :authentication => :plain,
-      :domain => 'localhost.localdomain'
-      }
-    })
-  end
+# def send_message
+#   Pony.mail({
+#     :from => params[:name] + "<" + params[:email] + ">",
+#     :to => 'jgarvey86@gmail.com',
+#     :subject => params[:name] + " has contacted you",
+#     :body => params[:message],
+#     # :port => '587',
+#     :via => :smtp,
+#     :via_options => {
+#       :address => 'smtp.gmail.com',
+#       :port => '587',
+#       :enable_starttls_auto => true,
+#       :user_name => 'jgarvey86',
+#       :password => 'cjohnson71$',
+#       :authentication => :plain,
+#       :domain => 'localhost.localdomain'
+#       }
+#     })
+#   end
 
 
 get '/' do
